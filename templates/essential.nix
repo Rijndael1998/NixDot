e476ba8c4fe44b72c44c74d50e5e7fbe34b74c46
Lukasz Baldyga <lukasz@baldy.ga>
2024-08-06 05:15:24 +0100

    spelling works

--- templates/essential.nix
+++ templates/essential.nix
@@ -6,15 +6,30 @@
     vscode = vscodium;
     vscodeExtensions = with vscode-extensions; [
       bbenoist.nix
-      ms-python.python
-      ms-azuretools.vscode-docker
-      ms-vscode-remote.remote-ssh
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
         name = "dscodegpt";
         publisher = "DanielSanMedium";
-        version = "3.4.10";
-        hash = "sha256-zjaM9YME0wfBOwhJTacnQbQvw35QL5NvXIBAx5d/bjI=";
+        version = "3.4.57";
+        hash = "sha256-yBjeUrj/DVTogN6IetwkVKjag9boeb/8SfZo0fG1NkQ=";
+      }
+      {
+        name = "code-spell-checker";
+        publisher = "streetsidesoftware";
+        version = "4.0.3";
+        hash = "sha256-CEGwbw5RpFsfB/g2inScIqWB7/3oxgxz7Yuc6V3OiHg=";
+      }
+      {
+        name = "code-spell-checker-british-english";
+        publisher = "streetsidesoftware";
+        version = "v1.4.10";
+        hash = "sha256-/Ezv6sHAhWy6XujB76tZVR7X5250BDXRQeypV0pqeBg=";
+      }
+      {
+        name = "vscode-mdx";
+        publisher = "unifiedjs";
+        version = "v1.8.9";
+        hash = "sha256-qj8tHc3dtvSFSPUeufvYy+rsa1Gb4tFYemiMDNJlEKg=";
       }
     ];
   })
