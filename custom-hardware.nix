778e30483072bf4ad2c9b71a6ec5edb604bf6a02
Rijndael <rijndael@baldy.ga>
2025-03-16 13:01:10 +0000

    added libva

--- custom-hardware.nix
+++ custom-hardware.nix
@@ -5,13 +5,14 @@
   # Enable openGL and install Rocm
   config.hardware.graphics = {
     enable = true;
-    #driSupport = true;
-    #driSupport32Bit = true;
     extraPackages = with pkgs; [
       rocmPackages.clr.icd
       rocmPackages.clr
       rocmPackages.rocminfo
       rocmPackages.rocm-runtime
+
+      # used for vaapi
+      libva
     ];
   };
 }
