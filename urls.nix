364acc0254a829192d2a2ab8d7d36b5962d45d43
Rijndael <rijndael@baldy.ga>
2025-05-27 10:55:11 +0100

    no small

--- urls.nix
+++ urls.nix
@@ -3,10 +3,6 @@
         url = "https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz";
     };
 
-    stable-small = fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/25.05-small.tar.gz";
-    };
-    
     unstable = fetchTarball { 
         url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
     };
