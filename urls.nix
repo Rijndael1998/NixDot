e445f77bdbfb3847fda7bf756c9ef2fa2e77062c
Rijndael <rijndael@baldy.ga>
2025-05-27 21:31:18 +0100

    flakes are more cancer than the nix lang itself

--- urls.nix
+++
@@ -1,9 +0,0 @@
-{}: {
-    stable = fetchTarball { 
-        url = "https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz";
-    };
-
-    unstable = fetchTarball { 
-        url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
-    };
-}
