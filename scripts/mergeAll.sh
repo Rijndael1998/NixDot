b6c267d80f1373434c35191a4abc70d77452296e
Rijndael <rijndael@baldy.ga>
2025-05-28 05:42:20 +0100

    added scripting stuffs

---
+++ scripts/mergeAll.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e 
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/../
+
+pairs=(
+    "master" "smol-latest"
+    "master" "omen"
+    "master" "server/master"
+    "server/master" "server/trusty"
+)
+
+for ((i=0; i<${#pairs[@]}; i+=2)); do
+    source="${pairs[i]}"
+    destination="${pairs[i+1]}"
+    ./scripts/merge.sh $source $destination &
+done
+
+wait
