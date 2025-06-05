9b9666769f32a3766c3b124a535b0c2354ef7817
Rijndael <rijndael@baldy.ga>
2025-06-05 12:32:50 +0100

    rebuilt

--- lambdas/nginx/reverse_with_headers.nix
+++ lambdas/nginx/reverse_with_headers.nix
@@ -1,20 +1,11 @@
-# TODO: This could call the reverse and then add on the extra config
-{ domain, proxyURL, extraLoc ? "", extraMain ? "" } : {
-  name = domain;
-  value = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = proxyURL;
-      proxyWebsockets = true;
-
-      extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-      '' + extraLoc;
-    };
-    extraConfig = extraMain;
-  };
-}
+{ domain, proxyURL, extraLoc ? "", extraMain ? "" } : (import ./reverse.nix {
+  domain = domain;
+  proxyURL = proxyURL;
+  extraLoc = ''
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+  '' + extraLoc;
+  extraMain = extraMain;
+})
