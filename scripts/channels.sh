5c2c704c2c1f1bfa5f82ac5c3f6c9a43bb4ac4f1
Rijndael <rijndael@baldy.ga>
2025-06-28 01:43:44 +0100

    remove musnix from server

--- scripts/channels.sh
+++ scripts/channels.sh
@@ -5,11 +5,9 @@ set -e
 # remove channels
 nix-channel --remove nixos
 nix-channel --remove nixos-unstable
-nix-channel --remove musnix
 
 # unstable seems to be more stable
 nix-channel --add https://nixos.org/channels/nixos-25.05                  nixos
 nix-channel --add https://nixos.org/channels/nixos-unstable               nixos-unstable
-nix-channel --add https://github.com/musnix/musnix/archive/master.tar.gz  musnix
 
 nix-channel --update
