f2d9bdc57511f160f36717f1556b7059ca428a12
Rijndael <rijndael@baldy.ga>
2025-07-21 23:16:45 +0100

    idk how else to make this work properly

--- lambdas/nginx/reverse_with_headers_and_ws.nix
+++ lambdas/nginx/reverse_with_headers_and_ws.nix
@@ -7,7 +7,7 @@
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto $scheme;
 
-    client_max_body_size 100000M;
+    client_max_body_size 100000G;
     proxy_read_timeout   600s;
     proxy_send_timeout   600s;
     send_timeout         600s;
