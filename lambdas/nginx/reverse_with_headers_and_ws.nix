7dde238620779de836b358f0a7bb544b8d8d13a4
Rijndael <rijndael@baldy.ga>
2025-07-21 22:21:02 +0100

    added proxy settings

--- lambdas/nginx/reverse_with_headers_and_ws.nix
+++ lambdas/nginx/reverse_with_headers_and_ws.nix
@@ -1,17 +1,13 @@
 { domain, proxyURL, extraLoc ? "", extraMain ? "" } : (import ./reverse.nix {
   domain = domain;
   proxyURL = proxyURL;
-
-  proxyWebsockets = true;
-  recommendedProxySettings = true;
-
   extraLoc = ''
     proxy_set_header Host $host;
     proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto $scheme;
 
-    client_max_body_size 500G;
+    client_max_body_size 0;
     proxy_read_timeout   600s;
     proxy_send_timeout   600s;
     send_timeout         600s;
