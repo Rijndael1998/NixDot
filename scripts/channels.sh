51dff370967202703998e093620093affd63f3b2
Rijndael <rijndael@baldy.ga>
2025-08-30 15:23:24 +0100

    unstable for all... building fails because reasons

--- scripts/channels.sh
+++ scripts/channels.sh
@@ -7,7 +7,7 @@ nix-channel --remove nixos
 nix-channel --remove nixos-unstable
 
 # unstable seems to be more stable
-nix-channel --add https://nixos.org/channels/nixos-25.05                  nixos
+nix-channel --add https://nixos.org/channels/nixos-unstable                  nixos
 nix-channel --add https://nixos.org/channels/nixos-unstable               nixos-unstable
 
 nix-channel --update
