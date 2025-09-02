dfc7fac0181910ea2a294ac4fbe406a63c7990eb
Rijndael <rijndael@baldy.ga>
2025-09-02 11:12:36 +0100

    added ai service

--- configuration.nix
+++ configuration.nix
@@ -462,7 +462,15 @@ in
   };
   services.archisteamfarm.ipcPasswordFile = "/etc/keys/asf/ipcpwd";
 
+  # AI
+  services.open-webui = {
+    enable = true;
+    port = 3010;
+    host = "0.0.0.0";
+    openFirewall = true;
+  };
 
+  # Auto Update
   systemd.timers."auto-update" = {
   wantedBy = [ "timers.target" ];
     timerConfig = {
