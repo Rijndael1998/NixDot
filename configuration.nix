50102e264f14b39f9b01ec09b8b72dd225c6f505
Rijndael <rijndael@baldy.ga>
2025-09-02 11:57:35 +0100

    sad

--- configuration.nix
+++ configuration.nix
@@ -463,12 +463,12 @@ in
   services.archisteamfarm.ipcPasswordFile = "/etc/keys/asf/ipcpwd";
 
   # AI
-  services.open-webui = {
-    enable = true;
-    port = 3010;
-    host = "0.0.0.0";
-    openFirewall = true;
-  };
+  # services.open-webui = {
+  #   enable = true;
+  #   port = 3010;
+  #   host = "0.0.0.0";
+  #   openFirewall = true;
+  # };
 
   # Auto Update
   systemd.timers."auto-update" = {
--- scripts/channels.sh
+++ scripts/channels.sh
@@ -7,7 +7,7 @@ nix-channel --remove nixos
 nix-channel --remove nixos-unstable
 
 # unstable seems to be more stable
-nix-channel --add https://nixos.org/channels/nixos-unstable-small               nixos
-nix-channel --add https://nixos.org/channels/nixos-unstable-small               nixos-unstable
+nix-channel --add https://nixos.org/channels/nixos-unstable               nixos
+nix-channel --add https://nixos.org/channels/nixos-unstable               nixos-unstable
 
 nix-channel --update
