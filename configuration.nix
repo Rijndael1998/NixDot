3cc3fcb32b79617d8fa49fc46112a67db76df526
Rijndael <rijndael@baldy.ga>
2025-09-13 16:56:51 +0100

    bluetooth support is enabled by default now

--- configuration.nix
+++ configuration.nix
@@ -271,9 +271,6 @@ ATTRS{idProduct}=="0204", ATTRS{idVendor}=="0d28", ENV{ID_MM_DEVICE_IGNORE}="1",
   # waydroid / android
   virtualisation.waydroid.enable = true;
 
-  # bluetooth
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-
   # Docker
   virtualisation.docker.enable = true;
   
