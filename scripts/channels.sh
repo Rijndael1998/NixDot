b26d2b5b4ddbd75700b6ece99ca5643ea4183948
Rijndael <rijndael@baldy.ga>
2025-10-07 20:50:15 +0100

    reverted to nixos-unstable

--- scripts/channels.sh
+++ scripts/channels.sh
@@ -8,8 +8,8 @@ nix-channel --remove nixos-unstable
 nix-channel --remove musnix
 
 # unstable seems to be more stable
-nix-channel --add https://nixos.org/channels/nixos-25.05               nixos
-nix-channel --add https://nixos.org/channels/nixos-25.05               nixos-unstable
+nix-channel --add https://nixos.org/channels/nixos-unstable               nixos
+nix-channel --add https://nixos.org/channels/nixos-unstable               nixos-unstable
 nix-channel --add https://github.com/musnix/musnix/archive/master.tar.gz  musnix
 
 nix-channel --update
