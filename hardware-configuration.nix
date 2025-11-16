f5c98884a41faf52815712d6d25af4599ddf5dd5
Rijndael <rijndael1998@baldy.ga>
2025-11-16 00:02:46 +0000

    updated hw config

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -14,33 +14,28 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4549521d-35cb-45da-aff8-5c63c56a21b2";
+    { device = "/dev/disk/by-uuid/597b6817-56d4-447f-b5ee-a2192b736234";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-daeb1cc4-8a25-45fd-9714-f6f908f470f9".device = "/dev/disk/by-uuid/daeb1cc4-8a25-45fd-9714-f6f908f470f9";
+  boot.initrd.luks.devices."luks-33293a09-90ca-4ce1-b736-9ce7850f3f14".device = "/dev/disk/by-uuid/33293a09-90ca-4ce1-b736-9ce7850f3f14";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/6654-235C";
+    { device = "/dev/disk/by-uuid/416E-BB82";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices = [ ];
-  environment.etc.crypttab = {
-    mode = "0600";
-    text = ''
-      #
-      crypttoaster  UUID=f366d91b-955d-4527-9f1b-d7f913c48260 /root/keys/2tb_toaster.key nofail
-      cryptexternal UUID=f20e3e7c-c606-409f-b68a-93f0d4e33b6a /root/keys/2tb_external.key nofail
-    '';
-  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
